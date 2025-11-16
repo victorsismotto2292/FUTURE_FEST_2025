39b7067ba3018b538be6c056b3595d9229039c75
Vecyn_SisMoTTO <vgr.sismotto@gmail.com>
2025-11-16 00:50:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3005;
 
 app.use('/public', express.static('public'));
 app.use('/style', express.static('style'));
